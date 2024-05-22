9e0927f76ded8f86b27fbea8d91ec2e9b5f80331
RomeroGabriel <gabrielromerods@hotmail.com>
2024-05-22 13:43:40 -0300

    more test in event process test

--- pkg/eventprocess/event_process_test.go
+++ pkg/eventprocess/event_process_test.go
@@ -14,3 +14,43 @@ func TestEventMessageEmpty(t *testing.T) {
 	err := result.ValidateEmptyFields()
 	assert.Error(t, err)
 }
+
+func TestMessageEmpty(t *testing.T) {
+	msg := EventMessage{
+		EventType: "user-app",
+		ClientId:  "client-3",
+		Message:   "Nice msg",
+	}
+	err := msg.ValidateEmptyFields()
+	assert.NoError(t, err)
+}
+
+func TestEventEmptyMessageEmpty(t *testing.T) {
+	msg := EventMessage{
+		EventType: "",
+		ClientId:  "client-3",
+		Message:   "Nice msg",
+	}
+	err := msg.ValidateEmptyFields()
+	assert.Error(t, err)
+}
+
+func TestClientEmptyMessageEmpty(t *testing.T) {
+	msg := EventMessage{
+		EventType: "user-app",
+		ClientId:  "",
+		Message:   "Nice msg",
+	}
+	err := msg.ValidateEmptyFields()
+	assert.Error(t, err)
+}
+
+func TestMessageEmptyMessageEmpty(t *testing.T) {
+	msg := EventMessage{
+		EventType: "user-app",
+		ClientId:  "client-3",
+		Message:   "",
+	}
+	err := msg.ValidateEmptyFields()
+	assert.Error(t, err)
+}
